8fdc4220147a96f3382da3c3ef6f8470849718ae
elielfilhodev <elielfilho9@outlook.com>
2025-03-20 18:25:46 -0300

    modified

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
+    navLinks.classList.toggle('active'); // Alterna a classe 'active'
 }
